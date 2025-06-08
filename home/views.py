373f0ea636b2b652919f31bd56583157928857e1
Vishnu Priya Kavya <127675676+Vishnupriyakavya@users.noreply.github.com>
2025-06-08 19:36:18 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -179,22 +179,6 @@ def owner_dashboard(request):
     context = {'orders': orders}
     return render(request, 'owner_dashboard.html', context)
 
-'''
-
-def update_order_status(request, order_uid):
-<<<<<<< HEAD
-=======
-    if request.method == 'POST':
-        try:
-            cart = Cart.objects.get(uid=order_uid)
-            new_status = request.POST.get('status')
-            if new_status in ['pending', 'completed']:
-                cart.status = new_status
-                cart.save()
-                messages.success(request, "Order status updated.")
-        except Cart.DoesNotExist:
-            messages.error(request, "Order not found.")
-    return redirect('owner_dashboard')'''
 
 @staff_member_required(login_url='/login/')
 def update_order_status(request, order_uid):
